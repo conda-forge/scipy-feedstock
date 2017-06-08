2bdf4d85e01bfc82aff8e2ce0bc0720f11fb3cdf
Isuru Fernando <isuru.11@cse.mrt.ac.lk>
2017-06-08 11:49:55 +0530

    Remove openblas hacks

--- recipe/build.sh
+++ recipe/build.sh
@@ -22,22 +22,4 @@ else
     unset LDFLAGS
 fi
 
-# If OpenBLAS is being used, we should be able to find the libraries.
-# As OpenBLAS now will have all symbols that BLAS or LAPACK have,
-# create libraries with the standard names that are linked back to
-# OpenBLAS. This will make it easy for NumPy to find it.
-test -f "${PREFIX}/lib/libopenblas.a" && ln -fs "${PREFIX}/lib/libopenblas.a" "${PREFIX}/lib/libblas.a"
-test -f "${PREFIX}/lib/libopenblas.a" && ln -fs "${PREFIX}/lib/libopenblas.a" "${PREFIX}/lib/liblapack.a"
-test -f "${PREFIX}/lib/libopenblas${SHLIB_EXT}" && ln -fs "${PREFIX}/lib/libopenblas${SHLIB_EXT}" "${PREFIX}/lib/libblas${SHLIB_EXT}"
-test -f "${PREFIX}/lib/libopenblas${SHLIB_EXT}" && ln -fs "${PREFIX}/lib/libopenblas${SHLIB_EXT}" "${PREFIX}/lib/liblapack${SHLIB_EXT}"
-
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
-
-# Need to clean these up as we don't want them as part of the NumPy package.
-# If these are part of a BLAS (e.g. ATLAS), this won't cause us any problems
-# as those would have been existing packages and `conda-build` would have
-# ignored packaging those files anyways.
-rm -f "${PREFIX}/lib/libblas.a"
-rm -f "${PREFIX}/lib/liblapack.a"
-rm -f "${PREFIX}/lib/libblas${SHLIB_EXT}"
-rm -f "${PREFIX}/lib/liblapack${SHLIB_EXT}"
