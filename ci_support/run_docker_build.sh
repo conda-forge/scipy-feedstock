8bf7997896cbd95272e88d10a18c084569812293
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-06-08 01:30:09 -0400

    MNT: Re-rendered with conda-smithy 2.3.1
    Add NumPy 1.13 to the Travis CI build matrix. On Linux switch from NumPy
    1.12 to 1.13. A separate branch has been created to maintain NumPy 1.12
    on Linux.

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -67,21 +67,21 @@ source run_conda_forge_build_setup
 
 # Embarking on 3 case(s).
     set -x
-    export CONDA_NPY=112
+    export CONDA_NPY=113
     export CONDA_PY=27
     set +x
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
     set -x
-    export CONDA_NPY=112
+    export CONDA_NPY=113
     export CONDA_PY=35
     set +x
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
     set -x
-    export CONDA_NPY=112
+    export CONDA_NPY=113
     export CONDA_PY=36
     set +x
     conda build /recipe_root --quiet || exit 1
