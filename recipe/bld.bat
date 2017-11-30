2053b91922174f6bc86cb5c638046e69803cc4f2
Isuru Fernando <isuruf@gmail.com>
2017-11-30 10:17:44 -0600

    Add windows support

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,17 +1,6 @@
-set COMP_DIR=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows
+REM Use OpenBLAS with 1 thread only as it seems to be using too many
+REM on the CIs apparently.
+set OPENBLAS_NUM_THREADS=1
 
-set DISTUTILS_USE_SDK=1
-set MSSdk=1
-
-if %PY_VER%==3.5 (
-REM This one is essential for getting DLL linkage on Py3.5+
-    set "PY_VCRUNTIME_REDIST=%LIBARARY_BIN%\vcruntime140.dll"
-    set VS=vs2015
-) else (
-    set VS=vs2010
-)
-
-call "%COMP_DIR%\bin\ifortvars.bat" %INTEL_ARCH% %VS%
-
-%PYTHON% setup.py install
+python setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
