7c20cfb28e747103ea593b46e6714dba59d973f6
Isuru Fernando <isuruf@gmail.com>
2017-11-30 23:41:28 -0600

    Use x86_amd64 compiler instead of amd64
    x86_amd64 is a 32-bit compiler producing amd64 code, while amd64 is
    a 64-bit compiler producing amd64 code. Latter gives a ICE
    while former does not

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
 REM Use OpenBLAS with 1 thread only as it seems to be using too many
 REM on the CIs apparently.
 set OPENBLAS_NUM_THREADS=1
-
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
 python setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
