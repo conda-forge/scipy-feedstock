415cc5f18afdc85cc3bcc36d2ba70a40a6081a84
Mark Harfouche <mark.harfouche@gmail.com>
2019-11-10 16:38:12 -0500

    Skip all but aarch64 to save resources

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,4 +9,7 @@ export OPENBLAS_NUM_THREADS=1
 # can have a G77 ABI (currently only MKL)
 export SCIPY_USE_G77_ABI_WRAPPER=1
 
-pip install . -vv
+# pip install . -vv
+# Build in parallel to help aarch finish slightly faster
+python setup.py build_ext --inplace -j4
+python setup.py install -vv
