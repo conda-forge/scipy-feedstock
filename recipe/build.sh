92f784e54f01c743b2c0e1ea36f719b8ae2588e0
Isuru Fernando <isuruf@gmail.com>
2020-03-30 10:51:02 -0500

    Try with -fno-optimize-sibling-calls

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,13 @@
 # can have a G77 ABI (currently only MKL)
 export SCIPY_USE_G77_ABI_WRAPPER=1
 
+if [[ "$target_platform" == linux* ]]; then
+    export CFLAGS="$CFLAGS -fno-optimize-sibling-calls"
+    export CXXFLAGS="$CXXFLAGS -fno-optimize-sibling-calls"
+    export CPPFLAGS="$CPPFLAGS -fno-optimize-sibling-calls"
+    export FFLAGS="$FFLAGS -fno-optimize-sibling-calls"
+fi
+
 if [[ "$python_impl" == "pypy" && "$target_platform" == "linux-ppc64le" ]]; then
     python setup.py install --single-version-externally-managed --record=record.txt
 else
