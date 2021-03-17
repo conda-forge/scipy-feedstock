891cb0bd4e3a8c653e61c75615947b07d62f1013
Eli Rykoff <erykoff@stanford.edu>
2021-03-16 22:11:10 -0700

    Remove unnecessary patch with updated version of gfortran.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,12 +4,6 @@
 # can have a G77 ABI (currently only MKL)
 export SCIPY_USE_G77_ABI_WRAPPER=1
 
-# When cross-compiling to osx-arm64, certain fortran optimizations cause seg-faults
-# https://github.com/iains/gcc-darwin-arm64/issues/44
-if [[ $target_platform == "osx-arm64" ]]; then
-  export FFLAGS="${FFLAGS} -fno-tree-loop-vectorize -fno-tree-slp-vectorize"
-fi
-
 if [[ "$python_impl" == "pypy" && "$target_platform" == "linux-ppc64le" ]]; then
     $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 else
