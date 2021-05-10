9af718e5d267a792e0224d035b9550772909fe68
H. Vetinari <h.vetinari@gmx.com>
2021-05-10 10:01:17 +0200

    remove obsolete test skips

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -16,11 +16,7 @@ if os.getenv("CI") != "travis":
 elif is_pypy:
     kwargs["extra_argv"].append("-n4")
 
-if sys.platform.startswith("linux"):
-    kwargs["extra_argv"].append("-k not test_curvefit_covariance")
-
 if os.getenv("CI") == "drone":
-    kwargs["extra_argv"].append("-k not (test_krandinit or test_heequb)")
     # Run only linalg tests on drone as drone timeouts
     kwargs['tests'] = ["scipy.linalg", "scipy.fft"]
 
