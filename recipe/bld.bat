a61bdb95f69962458676801a0a3b5f4060f6b48e
Isuru Fernando <isuruf@gmail.com>
2021-06-14 11:25:28 -0500

    Unset SRC_DIR on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -39,6 +39,8 @@ set "CXX=clang-cl"
 
 REM clang-cl & gfortran use different LDFLAGS; unset it
 set "LDFLAGS="
+REM don't add d1trimfile option because clang doesn't recognize it.
+set "SRC_DIR="
 
 %PYTHON% -m pip install . -vv
 if %ERRORLEVEL% neq 0 exit 1
