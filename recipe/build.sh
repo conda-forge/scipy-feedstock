9c881a9ccd1ebec032ba434fe62bc8f4c1bb47f5
mattip <matti.picus@gmail.com>
2021-07-18 22:20:01 +0300

    add set -x to debug script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 # Use the G77 ABI wrapper everywhere so that the underlying blas implementation
 # can have a G77 ABI (currently only MKL)
 export SCIPY_USE_G77_ABI_WRAPPER=1
