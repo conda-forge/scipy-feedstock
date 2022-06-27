871b1feae8d6dee4267b6afc741f338511a35f4a
H. Vetinari <h.vetinari@gmx.com>
2022-06-27 20:07:16 +0200

    go back to default build system (now meson) on unix

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-set -x
+set -ex
 
 # Use the G77 ABI wrapper everywhere so that the underlying blas implementation
 # can have a G77 ABI (currently only MKL)
 export SCIPY_USE_G77_ABI_WRAPPER=1
 
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON -m pip install . -vv
