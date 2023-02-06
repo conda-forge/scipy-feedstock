20393db97264740cbf8b63981d99db31dafa09ad
H. Vetinari <h.vetinari@gmx.com>
2023-02-06 16:00:28 +1100

    separate invocation of mesion and python -m build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -ex
 
+mkdir builddir
+
+# need to run meson first for cross-compilation case
+meson ${MESON_ARGS} builddir
+
 # -wnx flags mean: --wheel --no-isolation --skip-dependency-check
-$PYTHON -m build -w -n -x  -Csetup-args=-Dblas=blas -Csetup-args=-Dlapack=lapack -Csetup-args=-Duse-g77-abi=true
+$PYTHON -m build -w -n -x \
+    -Cbuilddir=builddir \
+    -Csetup-args=-Dblas=blas \
+    -Csetup-args=-Dlapack=lapack \
+    -Csetup-args=-Duse-g77-abi=true
 pip install dist/scipy*.whl
