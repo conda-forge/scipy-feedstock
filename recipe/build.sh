ea035e0d9576218e17a36ffe8f90fa0b8e22fda8
H. Vetinari <h.vetinari@gmx.com>
2023-02-10 13:53:16 +1100

    populate MESON_ARGS & manually add a meson_cross_file.txt for linux-*

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,20 @@ set -ex
 
 mkdir builddir
 
+# for some reason empty on linux
+export MESON_ARGS=${MESON_ARGS:---prefix=${PREFIX} --libdir=lib}
+
+if [[ "$target_platform" == "linux-aarch64" ]]; then
+    cp $RECIPE_DIR/meson_cross_aarch64.txt ${BUILD_PREFIX}/meson_cross_file.txt
+    export MESON_ARGS="${MESON_ARGS} --cross-file=${BUILD_PREFIX}/meson_cross_file.txt"
+elif [[ "$target_platform" == "linux-ppc64le" ]]; then
+    cp $RECIPE_DIR/meson_cross_ppc64le.txt ${BUILD_PREFIX}/meson_cross_file.txt
+    export MESON_ARGS="${MESON_ARGS} --cross-file=${BUILD_PREFIX}/meson_cross_file.txt"
+fi
+
+# debug
+cat ${BUILD_PREFIX}/meson_cross_file.txt || true
+
 # need to run meson first for cross-compilation case
 meson setup ${MESON_ARGS} \
     -Dblas=blas \
