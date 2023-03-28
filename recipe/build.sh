5014408b7fb88ece06d94508bc5b8e98c9ac7102
H. Vetinari <h.vetinari@gmx.com>
2023-03-28 18:56:05 +1100

    remove manual meson_cross_file.txt, now that compiler-activation takes care of it

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,28 +3,6 @@ set -ex
 
 mkdir builddir
 
-# for some reason empty on linux
-export MESON_ARGS=${MESON_ARGS:---prefix=${PREFIX} --libdir=lib}
-
-if [[ "$target_platform" == "linux-aarch64" ]]; then
-    cp $RECIPE_DIR/meson_cross_aarch64.txt ${BUILD_PREFIX}/meson_cross_file.txt
-    export MESON_ARGS="${MESON_ARGS} --cross-file=${BUILD_PREFIX}/meson_cross_file.txt"
-elif [[ "$target_platform" == "linux-ppc64le" ]]; then
-    cp $RECIPE_DIR/meson_cross_ppc64le.txt ${BUILD_PREFIX}/meson_cross_file.txt
-    export MESON_ARGS="${MESON_ARGS} --cross-file=${BUILD_PREFIX}/meson_cross_file.txt"
-elif [[ "$target_platform" == "osx-arm64" ]]; then
-    cp $RECIPE_DIR/meson_cross_arm64.txt ${BUILD_PREFIX}/meson_cross_file.txt
-    # already adds --cross-file to MESON_ARGS
-fi
-
-# debug
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
-    cat ${BUILD_PREFIX}/meson_cross_file.txt
-    sed -i 's#REPLACE_BUILD_PREFIX#'${BUILD_PREFIX}'#g' ${BUILD_PREFIX}/meson_cross_file.txt
-    sed -i 's#REPLACE_PREFIX#'${PREFIX}'#g' ${BUILD_PREFIX}/meson_cross_file.txt
-    cat ${BUILD_PREFIX}/meson_cross_file.txt
-fi
-
 # need to run meson first for cross-compilation case
 $PYTHON $(which meson) setup ${MESON_ARGS} \
     -Dblas=blas \
