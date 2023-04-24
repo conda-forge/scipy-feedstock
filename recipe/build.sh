db94f45496038d486167f24ef0b36945bfff385d
H. Vetinari <h.vetinari@gmx.com>
2023-04-24 11:52:16 +1100

    strip `--buildtype release` out from `MESON_ARGS`

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,12 +3,16 @@ set -ex
 
 mkdir builddir
 
+# meson-python already sets up a -Dbuildtype=release argument to meson, so
+# we need to strip --buildtype out of MESON_ARGS or fail due to redundancy
+MESON_ARGS_REDUCED="$(echo $MESON_ARGS | sed 's/--buildtype release //g')"
+
 # -wnx flags mean: --wheel --no-isolation --skip-dependency-check
 $PYTHON -m build -w -n -x \
     -Cbuilddir=builddir \
     -Csetup-args=-Dblas=blas \
     -Csetup-args=-Dlapack=lapack \
     -Csetup-args=-Duse-g77-abi=true \
-    -Csetup-args=${MESON_ARGS// / -Csetup-args=} \
+    -Csetup-args=${MESON_ARGS_REDUCED// / -Csetup-args=} \
     || (cat builddir/meson-logs/meson-log.txt && exit 1)
 pip install dist/scipy*.whl
