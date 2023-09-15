1c70927c66caac06887434fa2eb4b1bc628f60f3
H. Vetinari <h.vetinari@gmx.com>
2023-09-15 16:52:14 +1100

    according to meson, flang can't do `fortran_std=legacy`

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,5 +10,6 @@ set "FC=flang-new"
     -Cbuilddir=builddir ^
     -Csetup-args=-Dblas=blas ^
     -Csetup-args=-Dlapack=lapack ^
+    -Csetup-args=-Dfortran_std=none ^
     -Csetup-args=-Duse-g77-abi=true
 if %ERRORLEVEL% neq 0 (type builddir\meson-logs\meson-log.txt && exit 1)
