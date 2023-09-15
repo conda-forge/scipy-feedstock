8ba8310a2598c1742e4b00b3469f52e57ddacdf6
H. Vetinari <h.vetinari@gmx.com>
2023-09-15 17:31:53 +1100

    try without lld again

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,6 @@ mkdir builddir
 
 :: flang 17 still uses "temporary" name
 set "FC=flang-new"
-set "CLANG_DEFAULT_LINKER=lld"
 
 :: -wnx flags mean: --wheel --no-isolation --skip-dependency-check
 %PYTHON% -m build -w -n -x ^
