8e1c19e43a173ccc07a314b7144b666ce6133d46
H. Vetinari <h.vetinari@gmx.com>
2023-09-25 08:41:38 +1100

    set MESON_RSP_THRESHOLD

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,6 +7,10 @@ set "FC=flang-new"
 
 set "CFLAGS=%CFLAGS% -std=gnu99"
 
+:: see explanation here:
+:: https://github.com/conda-forge/scipy-feedstock/pull/253#issuecomment-1732578945
+set "MESON_RSP_THRESHOLD=32000"
+
 :: -wnx flags mean: --wheel --no-isolation --skip-dependency-check
 %PYTHON% -m build -w -n -x ^
     -Cbuilddir=builddir ^
