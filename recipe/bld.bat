1a1fa780f91b9c401304b4f3e1435ee647d18be2
H. Vetinari <h.vetinari@gmx.com>
2023-09-24 20:50:22 +1100

    ensure we're using MSVC-compatible clang-flavour

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,14 @@
 
 mkdir builddir
 
+:: check if clang-cl is on path as required
+clang-cl.exe --version
+if %ERRORLEVEL% neq 0 exit 1
+
+:: set compilers to clang-cl
+set "CC=clang-cl"
+set "CXX=clang-cl"
+
 :: flang 17 still uses "temporary" name
 set "FC=flang-new"
 
