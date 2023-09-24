2ef10eae3613388a4db057118755f24ba908c33a
H. Vetinari <h.vetinari@gmx.com>
2023-09-25 10:36:10 +1100

    mirror clang-win compiler activation
    flang uses `-Xflang` instead of `-Xclang`, but doesn't know any
    of the options we're using for clang and errors if specified.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,6 +13,13 @@ set "CXX=clang-cl"
 :: flang 17 still uses "temporary" name
 set "FC=flang-new"
 
+:: set up clang-cl correctly, see
+:: https://github.com/conda-forge/clang-win-activation-feedstock/blob/main/recipe/activate-clang_win-64.sh
+set "CPPFLAGS=-D_CRT_SECURE_NO_WARNINGS -D_MT -D_DLL --target=x86_64-pc-windows-msvc -nostdlib -Xclang --dependent-lib=msvcrt -fuse-ld=lld -fno-aligned-allocation"
+set "FFLAGS=-D_CRT_SECURE_NO_WARNINGS -D_MT -D_DLL --target=x86_64-pc-windows-msvc -nostdlib"
+set "LDFLAGS=--target=x86_64-pc-windows-msvc -nostdlib -Xclang --dependent-lib=msvcrt -fuse-ld=lld"
+set "LDFLAGS=%LDFLAGS% -Wl,-defaultlib:%LIBRARY_PREFIX%/lib/clang/17.0.0/lib/windows/clang_rt.builtins-x86_64.lib"
+
 :: -wnx flags mean: --wheel --no-isolation --skip-dependency-check
 %PYTHON% -m build -w -n -x ^
     -Cbuilddir=builddir ^
