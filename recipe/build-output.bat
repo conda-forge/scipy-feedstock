4ba3e40868accc91f9f6617c5664791b7262ff1f
H. Vetinari <h.vetinari@gmx.com>
2025-08-28 13:19:09 +1100

    leave flang activation to compiler package also in build-output.bat
    compare dbd7777f4d6e5da54472a04d38c3a6e3814ccc34

--- recipe/build-output.bat
+++ recipe/build-output.bat
@@ -28,22 +28,6 @@ if "%PKG_NAME%"=="scipy" (
     set "CC=clang-cl"
     set "CXX=clang-cl"
 
-    :: flang 17 still uses "temporary" name
-    set "FC=flang-new"
-
-    :: need to read clang version for path to compiler-rt
-    FOR /F "tokens=* USEBACKQ" %%F IN (`clang.exe -dumpversion`) DO (
-        SET "CLANG_VER=%%F"
-    )
-
-    :: attempt to match flags for flang as we set them for clang-on-win, see
-    :: https://github.com/conda-forge/clang-win-activation-feedstock/blob/main/recipe/activate-clang_win-64.sh
-    :: however, -Xflang --dependent-lib=msvcrt currently fails as an unrecognized option, see also
-    :: https://github.com/llvm/llvm-project/issues/63741
-    set "FFLAGS=-D_CRT_SECURE_NO_WARNINGS -D_MT -D_DLL --target=x86_64-pc-windows-msvc -nostdlib"
-    set "LDFLAGS=--target=x86_64-pc-windows-msvc -nostdlib -Xclang --dependent-lib=msvcrt -fuse-ld=lld"
-    set "LDFLAGS=%LDFLAGS% -Wl,-defaultlib:%BUILD_PREFIX%/Library/lib/clang/!CLANG_VER:~0,2!/lib/windows/clang_rt.builtins-x86_64.lib"
-
     :: see explanation here:
     :: https://github.com/conda-forge/scipy-feedstock/pull/253#issuecomment-1732578945
     set "MESON_RSP_THRESHOLD=320000"
